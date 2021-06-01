4a4a1c9ae472e065a08faf4497717a3a727cf345
raghavddps2 <raghav.ddps2@gmail.com>
2021-06-01 11:37:15 +0530

    a

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
 export const api1 = "https://secure-basin-28765.herokuapp.com"
-export const api2 = "http://fa071e1d9685.ngrok.io"
+export const api2 = "https://fathomless-brook-87233.herokuapp.com/"
 
 
